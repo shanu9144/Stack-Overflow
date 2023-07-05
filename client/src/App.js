cc49ff27dabcee5d64ab4eb55e45bef04f33f508
shanu9144 <sharmashantanu537@gmail.com>
2023-07-05 17:59:05 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import {BrowserRouter as Router} from 'react-router-dom'
+
+
+
+import './App.css';
+import Navbar from './Components/Navbar/Navbar'
+import AllRoutes from './AllRoutes'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+       <Navbar />
+       <AllRoutes />
+      </Router>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Questions/DispalyQuestion.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import LeftSidebar from '../../Components/LeftSidebar/LeftSidebar'
+import RightSidebar from '../../Components/RightSidebar/RightSidebar'
+import QuestionsDetails from './QuestionsDetails'
+
+
+const DispalyQuestion = () => {
+  return (
+    <div className='home-container-1'>
+      <LeftSidebar />
+      <div className='home-container-2'>
+        <QuestionsDetails />
+        <RightSidebar />
+      </div>
+    </div>  
+
+  )
+}
+
+export default DispalyQuestion
